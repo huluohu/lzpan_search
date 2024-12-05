61a1426aadd5de58055a452d3af4e1f6b0239c14
gxl <fooololo@gmail.com>
2024-12-05 17:56:23 +0800

    🚗解决Invalid Host header的问题

--- webpack.config.js
+++ webpack.config.js
@@ -68,7 +68,8 @@ module.exports = {
     devServer: {
         host: '0.0.0.0',
         port: 3000,
-        allowedHosts: [],
-        open: true
+        open: true,
+        compress: true,
+        disableHostCheck: true,
     }
 }
